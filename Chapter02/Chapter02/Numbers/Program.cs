2e7ef6bab2909eeafbbd5058e497e17ffebab178
Erekle Khomasuridze <ereklekhomasuridze@gmail.com>
2023-04-28 19:06:44 +0400

    Added Chapter01 and part of the Chapter02

---
+++ Chapter02/Chapter02/Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// unsigned integer means positive whole number or 0
+uint naturalNumber = 23;
+
+// integer means negative or positive whole number or 0
+int integerNumber = -23;
+
+// float means single-precision floating point
+// F suffix makes it a float literal
+float realNumber = 2.3F;
+
+// double means double-precision floating point
+// double is the default type for a number value with a decimal point .
+double anotherRealNumber = 2.3; // double literal
+
+// three variables that store the number 2 million
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+Console.WriteLine(hexadecimalNotation);
+// check the three variables have the same value
+// both statements output true
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
---
+++ Chapter02/Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,26 @@
+﻿//double heightInMetres = 1.88;
+//Console.WriteLine($"The variable {nameof(heightInMetres)} has the value { heightInMetres}.");
+
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
+//string grinningEmoji = char.ConvertFromUtf32(0x1F600);
+//Console.WriteLine(grinningEmoji.Length);
+
+//string filePath = @"C:\televisions\sony\bravia.txt";
+
+//string xml = """
+//             <person age="50">
+//                    <first_name>Mark</first_name>
+//             </person>
+//         """;
+
+////Console.WriteLine(xml);
+
+//var person = new { FirstName = "Alice", Age = 56 };
+//string json = $$"""
+// {
+//     "first_name": "{{person.FirstName}}",
+//     "age": {{person.Age}},
+//     "calculation", "{{{1 + 2}}}"
+// }
+// """;
+//Console.WriteLine(json);
