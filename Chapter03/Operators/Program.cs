283e72a20d435638309157f4817c2c6f378f384d
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-04-30 00:06:37 +0400

    Added Operators project in Chapter03.

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+int a1 = 3;
+int b1 = ++a1;
+WriteLine($"a is {a1}, b is {b1}");
+
+//Binary operators
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+//Assignment operators
+int p = 6;
+p += 3; // equivalent to p = p + 3;
+p -= 3; // equivalent to p = p - 3;
+p *= 3; // equivalent to p = p * 3;
+p /= 3; // equivalent to p = p / 3;
+
