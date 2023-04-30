18f7e8c5127e8528d9311d7a5d4b131de749f8e6
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-04-30 16:53:03 +0400

    Finished Arrays project in Chapter03.

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,20 @@
+﻿//int x = 0;
+//while (x < 10)
+//{
+//    WriteLine(x);
+//    x++;
+//}
+
+//string? password;
+//do
+//{
+//    Write("Enter your password: ");
+//    password = ReadLine();
+//}
+//while (password != "Pa$$w0rd");
+//WriteLine("Correct!");
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
