8886f1827eea76e33a26cb55978740ec46a33067
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-03 16:04:02 +0400

    Finished logging via appsettings in Chapter04.

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
 
@@ -10,5 +11,24 @@ Trace.Listeners.Add(logFile);
 
 Trace.AutoFlush = true;
 
-Debug.WriteLine("Debug says, I am watching!");
-Trace.WriteLine("Trace says, I am watching!");
+Console.WriteLine("Reading settings from appsettings.json in {0}", Directory.GetCurrentDirectory());
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("appsettings.json", 
+    optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+    displayName: "PacktSwitch", 
+    description: "This switch is set with JSON config");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace error!");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning!");
+Trace.WriteLineIf(ts.TraceInfo, "Trace info!");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose!");
+
+Console.ReadLine();
