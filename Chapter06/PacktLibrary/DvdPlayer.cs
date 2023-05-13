dc925b336d3cd2e78018b1d07278198f963347e9
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-13 11:57:15 +0400

    Added DvdPlayer and IPlayable in Chapter06.

---
+++ Chapter06/PacktLibrary/DvdPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Shared;
+
+public class DvdPlayer : IPlayable
+{
+    public void Pause()
+    {
+        WriteLine("DVD player is pausing.");
+    }
+
+    public void Play()
+    {
+        WriteLine("DVD player is playing.");
+    }
+}
