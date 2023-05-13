7ed6cc88f24f528cce25fa09f6c73f446f1eed81
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-13 20:14:01 +0400

    Added NullHandling project in Chapter06.

---
+++ Chapter06/NullHandling/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packed.Shared;
+
+public class Address
+{
+    public string? Building;
+    public string Street = string.Empty;
+    public string City = string.Empty;
+    public string Region = string.Empty;
+}
---
+++ Chapter06/NullHandling/Program.cs
@@ -0,0 +1,20 @@
+﻿//int? thisCouldBeNull = null;
+//WriteLine(thisCouldBeNull);
+//WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+//Nullable<int> thisCouldAlsoBeNull = null;
+//thisCouldAlsoBeNull = 9;
+//WriteLine(thisCouldAlsoBeNull);
+
+using Packed.Shared;
+
+Address address = new()
+{
+    Building = null,
+    Street = null!,
+    City = "London",
+    Region = "UK"
+};
+
+WriteLine(address.Building?.Length);
+WriteLine(address.Street.Length);
