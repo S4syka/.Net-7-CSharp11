95ab16c54a32a7df7de45da444b723f01a1e18c6
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-16 18:39:40 +0400

    Configured StyleCop.Analyzer in Chapter06

---
+++ Chapter06/CodeAnalyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="Bekeko">
+// Copyright (c) Bekeko. None of the rights reserved.
+// </copyright>
+
+using System.Diagnostics;
+
+namespace CodeAnalyzing;
+
+/// <summary>
+/// The main class for this console app.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// The main entry point for this console app.
+    /// </summary>
+    /// <param name="args">
+    /// A string array of arguments passed to the console app.
+    /// </param>
+    private static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello, Debugger!");
+    }
+}
