afa46174424da9a8294c34b7026a35804b44c7f2
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-23 20:12:37 +0400

    Added StreamWriter in Chapter09.

---
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+using WorkingWithStreams;
+using static System.Environment;
+using static System.IO.Path;
+
+SectionTitle("Writing to text streams");
+
+string textFile = Combine(CurrentDirectory, "Streams.txt");
+
+StreamWriter text = File.CreateText(textFile);
+
+foreach(string item in Viper.Callsigns)
+{
+    text.WriteLine(item);
+}
+
+text.Close();
+
+WriteLine("{0} contains {1:N0} bytes.", textFile, new FileInfo(textFile).Length);
+
+WriteLine(File.ReadAllText(textFile));
---
+++ Chapter09/WorkingWithStreams/Viper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStreams
+{
+    public static class Viper
+    {
+        public static string[] Callsigns = new[]
+        {
+            "Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack"
+        };
+
+
+    }
+}
