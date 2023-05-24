18f3457f966e142f4875dfc7c70c6b35067497ff
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-24 19:56:59 +0400

    Finished serialization using System.Text.Json in Chapter09.

---
+++ Chapter09/WorkingWIthJson/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string? Author { get; set; }
+
+    public Book(string title)
+    {
+        Title = title;
+    }
+
+    [JsonInclude] // include this field
+    public DateTime PublishDate;
+
+    [JsonInclude] // include this field
+    public DateTimeOffset Created;
+
+    public ushort Pages;
+}
---
+++ Chapter09/WorkingWIthJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using System.Text.Json; // JsonSerializer
+
+using static System.Environment;
+using static System.IO.Path;
+
+Book mybook = new("Red and Black")
+{
+    Author = "Stendhal",
+    PublishDate = DateTime.Now,
+    Pages = 42069,
+    Created = DateTimeOffset.Now
+};
+
+JsonSerializerOptions options = new()
+{
+    IncludeFields = true,
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+
+string filePath = Combine(CurrentDirectory, "mybook.json");
+
+using (Stream FileStream = File.Create(filePath))
+{
+    JsonSerializer.Serialize<Book>(FileStream, mybook, options);
+}
+
+Console.WriteLine("Written {0:N0} bytes to JSON to {1}", new FileInfo(filePath).Length, filePath);
+
+Console.WriteLine(File.ReadAllText(filePath));
