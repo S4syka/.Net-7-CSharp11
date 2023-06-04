6f219a70cc675982f160a5ecf69f021e3a6e7953
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-06-04 15:15:03 +0400

    Added examples of deffered execution in Chapter11.

---
+++ Chapter11/LinqWithObjects/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+
+SectionTitle("Deffered execution");
+
+var query1 = names
+    ////.Where(NameLongerThanFour)
+    .OrderBy(name => name.Length)
+    .ThenBy(name => name);
+
+var query2 = names
+    .Order();
+
+foreach(var item in query1)
+{
+    Console.WriteLine(item);
+}
+
