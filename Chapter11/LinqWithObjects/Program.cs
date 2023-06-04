51aa8a613ff56088fd992f36c356c9a1332541c3
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-06-04 15:23:03 +0400

    Added Filtering by types in Chapter11.

--- Chapter11/LinqWithObjects/Program.cs
+++ Chapter11/LinqWithObjects/Program.cs
@@ -1,17 +1,39 @@
-﻿string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+﻿//string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
 
-SectionTitle("Deffered execution");
+//SectionTitle("Deffered execution");
 
-var query1 = names
-    ////.Where(NameLongerThanFour)
-    .OrderBy(name => name.Length)
-    .ThenBy(name => name);
+//var query1 = names
+//    ////.Where(NameLongerThanFour)
+//    .OrderBy(name => name.Length)
+//    .ThenBy(name => name);
 
-var query2 = names
-    .Order();
+//var query2 = names
+//    .Order();
 
-foreach(var item in query1)
+//foreach(var item in query1)
+//{
+//    Console.WriteLine(item);
+//}
+
+SectionTitle("Filtering by type");
+List<Exception> exceptions = new()
+{
+ new ArgumentException(),
+ new SystemException(),
+ new IndexOutOfRangeException(),
+ new InvalidOperationException(),
+ new NullReferenceException(),
+ new InvalidCastException(),
+ new OverflowException(),
+ new DivideByZeroException(),
+ new ApplicationException()
+};
+
+var arithmeticExceptionsQuery = exceptions.OfType<ArithmeticException>();
+
+foreach (var item in arithmeticExceptionsQuery)
 {
     Console.WriteLine(item);
 }
+
 
