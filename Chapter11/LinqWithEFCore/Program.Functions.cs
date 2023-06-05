6905260622c787d4565eca123717f262a8267a62
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-06-05 17:27:07 +0400

    Created DataContext and Entity classes.

--- Chapter11/LinqWithEFCore/Program.Functions.cs
+++ Chapter11/LinqWithEFCore/Program.Functions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Packt.Shared;
 
 partial class Program
@@ -60,7 +62,7 @@ partial class Program
             var queryGroup = db.Categories.AsEnumerable().GroupJoin(db.Products, c => c.CategoryId, p => p.CategoryId,
                 (c, matchingProducts) => new { c.CategoryName, Products = matchingProducts.OrderBy(p => p.ProductName) });
 
-            foreach(var category in queryGroup)
+            foreach (var category in queryGroup)
             {
                 WriteLine("{0} has {1} products.",
                     arg0: category.CategoryName,
@@ -70,6 +72,69 @@ partial class Program
                 {
                     WriteLine($" {product.ProductName}");
                 }
+            }
+        }
+    }
+
+    static void OutputTableOfProducts(Product[] products, int currentPage, int totalPages)
+    {
+        string line = new('-', count: 73);
+        string lineHalf = new('-', count: 30);
+
+        WriteLine(line);
+        WriteLine("{0,4} {1,-40} {2,12} {3,-15}", "ID", "Product Name", "Unit Price", "Discontinued");
+        WriteLine(line);
+
+        foreach (Product p in products)
+        {
+            WriteLine("{0,4} {1,-40} {2,12:C} {3,-15}",
+            p.ProductId, p.ProductName, p.UnitPrice, p.Discontinued);
+        }
+
+        WriteLine("{0} Page {1} of {2} {3}", lineHalf, currentPage + 1, totalPages + 1, lineHalf);
+    }
+
+    static void OutputPageOfProducts(IQueryable<Product> products, int pageSize, int currentPage, int totalPages)
+    {
+        // We must order data before skipping and taking to ensure
+        // the data is not randomly sorted in each page.
+        var pagingQuery = products.OrderBy(p => p.ProductId)
+                            .Skip(currentPage * pageSize)
+                            .Take(pageSize);
+
+        SectionTitle(pagingQuery.ToQueryString());
+
+        OutputTableOfProducts(pagingQuery.ToArray(), currentPage, totalPages);
+    }
+
+    static void PagingProducts()
+    {
+        SectionTitle("Paging products");
+        using (Northwind db = new())
+        {
+            int pageSize = 10;
+            int currentPage = 0;
+            int productCount = db.Products.Count();
+            int totalPages = productCount / pageSize;
+            while (true)
+            {
+                OutputPageOfProducts(db.Products, pageSize, currentPage, totalPages);
+                Write("Press <- to page back, press -> to page forward, any key to exit.");
+
+                ConsoleKey key = ReadKey().Key;
+                if (key == ConsoleKey.LeftArrow)
+                    if (currentPage == 0)
+                        currentPage = totalPages;
+                    else
+                        currentPage--;
+                else if (key == ConsoleKey.RightArrow)
+                    if (currentPage == totalPages)
+                        currentPage = 0;
+                    else
+                        currentPage++;
+                else
+                    break; // out of the while loop.
+                WriteLine();
             }
         }
     }
--- Chapter11/LinqWithEFCore/Program.cs
+++ Chapter11/LinqWithEFCore/Program.cs
@@ -2,4 +2,6 @@
 
 //JoinCategoriesAndProducts();
 
-GroupJoinCategoriesAndProducts();
+//GroupJoinCategoriesAndProducts();
+
+PagingProducts();
