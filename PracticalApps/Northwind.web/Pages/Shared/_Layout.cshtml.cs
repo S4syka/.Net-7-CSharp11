872ec60c7b01367fd9d9f50c70cafdb72b854ba0
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-06-13 14:37:04 +0400

    Added _Layout

---
+++ PracticalApps/Northwind.web/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.web.Pages.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
